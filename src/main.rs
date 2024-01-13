b3e8137c62e018495425801cc694fe6834b3da2d
JethroNatividad <natividad.jet@gmail.com>
2024-01-13 23:01:19 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // program that takes in three numbers, if all numbers are the same, end the program, if not, show the largest number
+// Inputs: n1, n2, n3
+// Processs: check if equal, get largest number
+// Output: The largest number is {}.
 
 fn main() {
     println!("Hello, world!");
